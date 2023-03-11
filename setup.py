fd86f7b2e251fa7274a08daf16cf54ac3bc4f021
Michal Staniaszek <m.staniaszek@gmail.com>
2023-03-11 18:11:57 +0000

    delete everything except for what is needed by the wrapper, move it to spot_wrapper package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="spot_wrapper",
+    version="1.0.0",
+    description="Wrapper for Boston Dynamics Spot SDK",
+    packages=["spot_wrapper"],
+    package_dir={"": "src"},
+)
