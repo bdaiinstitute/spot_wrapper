105e6e97a6dfd9b53359b222da616bb1b9f3fcc1
Michal Staniaszek <m.staniaszek@gmail.com>
2023-03-11 18:59:32 +0000

    rename wrapper, match setup py

--- setup.py
+++ setup.py
@@ -1,33 +1,9 @@
-from setuptools import setup
-import os
-from glob import glob
-
-package_name = 'spot_driver'
+from setuptools import setup, find_packages
 
 setup(
-    name=package_name,
-    version='0.0.0',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='max',
-    maintainer_email='maximillian.kirsch@alumni.fh-aachen.de',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'spot_ros2 = spot_driver.spot_ros2:main',
-            'command_spot = spot_driver.command_spot_driver:main',
-            'spot_publish_cameras = spot_driver.publish_cameras:main',
-        ],
-    },
+    name="spot_wrapper",
+    version="1.0.0",
+    description="Wrapper for Boston Dynamics Spot SDK",
+    packages=["spot_wrapper"],
+    install_requires=["bosdyn-client", "bosdyn-api", "bosdyn-mission", "bosdyn-core"],
 )
