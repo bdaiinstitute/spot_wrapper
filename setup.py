0097875fad09372ac62e1ff96a6f1397a0ea5c72
jeremysee2 <32976023+jeremysee2@users.noreply.github.com>
2023-04-18 21:31:50 +0000

    add test script

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setup(
     version="1.0.0",
     description="Wrapper for Boston Dynamics Spot SDK",
     packages=["spot_wrapper"],
+    scripts=[
+        "tests/test_graph_nav_util.py",
+    ],
     install_requires=["bosdyn-client", "bosdyn-api", "bosdyn-mission", "bosdyn-core"],
 )
