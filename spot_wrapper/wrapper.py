875ea8149135746ba64b5998de99f91285efc736
Kaiyu Zheng <kzheng@theaiinstitute.com>
2023-05-09 13:18:26 -0400

    revert back to hard-coded strings

--- spot_wrapper/wrapper.py
+++ spot_wrapper/wrapper.py
@@ -119,79 +119,53 @@ ImageWithHandBundle = namedtuple(
     "ImageBundle", ["frontleft", "frontright", "left", "right", "back", "hand"]
 )
 
+IMAGE_SOURCES_BY_CAMERA = {
+    "frontleft": {
+        "visual": "frontleft_fisheye_image",
+        "depth": "frontleft_depth",
+        "depth_registered": "frontleft_depth_in_visual_frame"
+    },
+    "frontright": {
+        "visual": "frontright_fisheye_image",
+        "depth": "frontright_depth",
+        "depth_registered": "frontright_depth_in_visual_frame"
+    },
+    "left": {
+        "visual": "left_fisheye_image",
+        "depth": "left_depth",
+        "depth_registered": "left_depth_in_visual_frame"
+    },
+    "right": {
+        "visual": "right_fisheye_image",
+        "depth": "right_depth",
+        "depth_registered": "right_depth_in_visual_frame"
+    },
+    "back": {
+        "visual": "back_fisheye_image",
+        "depth": "back_depth",
+        "depth_registered": "back_depth_in_visual_frame"
+    },
+    "hand": {
+        "visual": "hand_color_image",
+        "depth": "hand_depth",
+        "depth_registered": "hand_depth_in_color_frame"
+    }
+}
+
 # Added to support getting images for given cameras
 Camera = Enum("Camera", ["frontleft", "frontright", "left", "right", "back", "hand"])
 ImageType = Enum("ImageType", ["visual", "depth", "depth_registered"])
-ImageSource = Enum(
-    "ImageSource",
-    [
-        "frontleft_fisheye_image",
-        "frontleft_depth",
-        "frontleft_depth_in_visual_frame",
-        "frontright_fisheye_image",
-        "frontright_depth",
-        "frontright_depth_in_visual_frame",
-        "left_fisheye_image",
-        "left_depth",
-        "left_depth_in_visual_frame",
-        "right_fisheye_image",
-        "right_depth",
-        "right_depth_in_visual_frame",
-        "back_fisheye_image",
-        "back_depth",
-        "back_depth_in_visual_frame",
-        "hand_color_image",
-        "hand_depth",
-        "hand_depth_in_color_frame",
-    ],
-)
-
 
 @dataclass(frozen=True, eq=True)
 class CameraSource:
     camera: Camera
     image_types: list[ImageType]
 
-
 @dataclass(frozen=True)
 class ImageEntry:
     camera_name: str
     image_type: ImageType
     image_response: image_pb2.ImageResponse
-
-
-IMAGE_SOURCES_BY_CAMERA = {
-    Camera.frontleft: {
-        ImageType.visual: ImageSource.frontleft_fisheye_image,
-        ImageType.depth: ImageSource.frontleft_depth,
-        ImageType.depth_registered: ImageSource.frontleft_depth_in_visual_frame,
-    },
-    Camera.frontright: {
-        ImageType.visual: ImageSource.frontright_fisheye_image,
-        ImageType.depth: ImageSource.frontright_depth,
-        ImageType.depth_registered: ImageSource.frontright_depth_in_visual_frame,
-    },
-    Camera.left: {
-        ImageType.visual: ImageSource.left_fisheye_image,
-        ImageType.depth: ImageSource.left_depth,
-        ImageType.depth_registered: ImageSource.left_depth_in_visual_frame,
-    },
-    Camera.right: {
-        ImageType.visual: ImageSource.right_fisheye_image,
-        ImageType.depth: ImageSource.right_depth,
-        ImageType.depth_registered: ImageSource.right_depth_in_visual_frame,
-    },
-    Camera.back: {
-        ImageType.visual: ImageSource.back_fisheye_image,
-        ImageType.depth: ImageSource.back_depth,
-        ImageType.depth_registered: ImageSource.back_depth_in_visual_frame,
-    },
-    Camera.hand: {
-        ImageType.visual: ImageSource.hand_color_image,
-        ImageType.depth: ImageSource.hand_depth,
-        ImageType.depth_registered: ImageSource.hand_depth_in_color_frame,
-    },
-}
 
 
 def robotToLocalTime(timestamp, robot):
