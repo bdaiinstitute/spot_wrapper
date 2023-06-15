1a5a43a6d6ebbc97cf4144b885431e6973cec8e9
Michal Staniaszek <m.staniaszek@gmail.com>
2023-06-15 18:04:36 +0100

    add get_screen function to cam wrapper, shows the name of the current screen

--- spot_wrapper/cam_wrapper.py
+++ spot_wrapper/cam_wrapper.py
@@ -154,7 +154,7 @@ class CompositorWrapper:
         """
         return self.client.get_visible_cameras()
 
-    def set_screen(self, screen):
+    def set_screen(self, screen: str):
         """
         Set the screen to be streamed over the network
 
@@ -162,6 +162,15 @@ class CompositorWrapper:
             screen: Screen to show
         """
         self.client.set_screen(screen)
+
+    def get_screen(self) -> str:
+        """
+        Get the screen currently being streamed over the network
+
+        Returns:
+            Name of the currently displayed screen
+        """
+        return self.client.get_screen().name
 
     def set_ir_colormap(self, colormap, min_temp, max_temp, auto_scale=True):
         """
