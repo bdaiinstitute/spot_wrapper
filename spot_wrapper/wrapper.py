a1b9c971d837e3a16a5f056b4aa5fa804573f979
Michal Staniaszek <m.staniaszek@gmail.com>
2023-07-20 13:57:56 +0100

    remove mistakenly merged robot command data and state

--- spot_wrapper/wrapper.py
+++ spot_wrapper/wrapper.py
@@ -505,36 +505,6 @@ class AsyncEStopMonitor(AsyncPeriodicQuery):
             pass
 
 
-@dataclass()
-class RobotState:
-    """
-    Dataclass which stores information about the robot's state. The values in it may be changed by methods
-    """
-
-    is_sitting: bool = True
-    is_standing: bool = False
-    is_moving: bool = False
-    at_goal: bool = False
-    near_goal: bool = False
-
-
-@dataclass()
-class RobotCommandData:
-    """
-    Store data about the commands the wrapper sends to the SDK. Running a command returns an integer value
-    representing that command's ID. These values are used to monitor the progress of the command and modify attributes
-    of RobotState accordingly. The values should be reset to none when the command completes.
-    """
-
-    last_stand_command: typing.Optional[int] = None
-    last_sit_command: typing.Optional[int] = None
-    last_docking_command: typing.Optional[int] = None
-    last_trajectory_command: typing.Optional[int] = None
-    # Was the last trajectory command requested to be precise
-    last_trajectory_command_precise: typing.Optional[bool] = None
-    last_velocity_command_time: typing.Optional[float] = None
-
-
 class SpotWrapper:
     """Generic wrapper class to encompass release 1.1.4 API features as well as maintaining leases automatically"""
 
