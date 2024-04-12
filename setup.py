f6fd872754df7a34da366457d96d36d0bd0264b9
Michel Hidalgo <mhidalgo@theaiinstitute.com>
2024-04-12 17:47:18 -0300

    Install certificates and key
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version="1.0.0",
     description="Wrapper for Boston Dynamics Spot SDK",
     packages=find_packages(include=["spot_wrapper*"]),
+    package_data={"spot_wrapper.testing.credentials": ["*.crt", "*.pem"]},
     install_requires=["bosdyn-client", "bosdyn-api", "bosdyn-mission", "bosdyn-core", "grpcio", "inflection", "pytest"],
 )
